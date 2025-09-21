5032d41f1598ce76c6ca2c48fa6f2af8ce7010f5
Manu Sharma <sharmamanu2727@gmail.com>
2025-09-21 23:43:05 +0530

    building and managing custom parameters

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'my_package'
@@ -12,19 +14,21 @@ setup(
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', [
             'launch/talker.launch.py',
-            'launch/listener.launch.py'
+            'launch/listener.launch.py',
+            'launch/python_parameters_launch.py'
         ])
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'rclpy'],
     zip_safe=True,
     maintainer='manu',
     maintainer_email='sharmamanu2727@gmail.com',
-    description='Testing Python Launch Files',
+    description='Learning ROS',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'my_node = my_package.my_node:main'
+            'my_node = my_package.my_node:main',
+            'minimal_param_node = my_package.python_parameters_node:main',
         ],
     },
 )
