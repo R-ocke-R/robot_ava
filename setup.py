2db4d88d50b1d1992ee5bcdfbb35a15874a63951
Manu Sharma <sharmamanu2727@gmail.com>
2025-09-22 16:43:46 +0530

    creating launch files

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import find_packages, setup
 
 package_name = 'my_package'
 
+def package_files(directory, pattern="*"):
+    return [f for f in glob(os.path.join(directory, pattern)) if os.path.isfile(f)]
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -12,11 +15,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', [
-            'launch/talker.launch.py',
-            'launch/listener.launch.py',
-            'launch/python_parameters_launch.py'
-        ])
+        (os.path.join('share', package_name, 'launch'),
+        package_files('launch')),
+        (os.path.join('share', package_name, 'config'),
+        package_files('config', "*.yaml")),
+        (os.path.join('share', package_name, 'rviz'),
+        package_files('rviz', "*.rviz")),
+
     ],
     install_requires=['setuptools', 'rclpy'],
     zip_safe=True,
@@ -24,7 +29,6 @@ setup(
     maintainer_email='sharmamanu2727@gmail.com',
     description='Learning ROS',
     license='Apache-2.0',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'my_node = my_package.my_node:main',
